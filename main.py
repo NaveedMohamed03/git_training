e665aef14264d856a6f7759869c509459b0169ea
NaveedMohamed03 <naveed@anorasolutions.com>
2023-03-21 17:37:56 +0530

    added sum function in main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 """
     Module: main.py
 """
+
+
+def addition(a, b):
+    return a + b
+
+
+print("sum of number : ", addition(4, 2))
+
 if __name__ == "__main__":
     print("menu driven program")
